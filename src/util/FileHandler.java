ce65acb70d20c90c8d6025b990213796e9edc97e
Line Guld <linekguld@hotmail.com>
2020-12-12 16:26:36 +0100

    Alle vores lister kan laves til JSON filer nu

--- src/util/FileHandler.java
+++ src/util/FileHandler.java
@@ -1,11 +1,8 @@
 package util;
 
-import Model.Project;
-import Model.Requirement;
-import Model.TeamMember;
+import Model.*;
 import parser.ParserException;
 import parser.XmlJsonParser;
-import Model.ProjectList;
 
 import java.io.*;
 
@@ -16,6 +13,9 @@ public class FileHandler
   // En metode der gemmer vores projektliste til en XML, som kan loades ind på hjemmesiden.
   public static void saveXML(ProjectList projects)
   {
+
+    //TODO
+
     XmlJsonParser theParser = new XmlJsonParser();
     File XMLfile;
     try
@@ -120,7 +120,39 @@ public class FileHandler
     File file;
     try
     {
-      file = jParser.toJson(projects.getProjects() , "allData.json");
+      file = jParser.toJson(projects.getProjects() , "allProjects.json");
+    }
+    catch (ParserException e)
+    {
+      System.out.println("JSon parser error");
+      e.printStackTrace();
+    }
+  }
+
+  public static void saveJson(CustomerList customers)
+  {
+    XmlJsonParser jParser = new XmlJsonParser();
+
+    File file;
+    try
+    {
+      file = jParser.toJson(customers.getCustomers() , "allCustomers.json");
+    }
+    catch (ParserException e)
+    {
+      System.out.println("JSon parser error");
+      e.printStackTrace();
+    }
+  }
+
+  public static void saveJson(EmployeeList employees)
+  {
+    XmlJsonParser jParser = new XmlJsonParser();
+
+    File file;
+    try
+    {
+      file = jParser.toJson(employees.getEmployees() , "allEmployees.json");
     }
     catch (ParserException e)
     {
