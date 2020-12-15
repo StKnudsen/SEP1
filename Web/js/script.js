e91cc0fa675d241b2331a328c3e3ce1143941c29
Tina Hadberg <tina.2000@live.dk>
2020-12-15 22:01:05 +0100

    XML kriser

--- Web/js/script.js
+++ Web/js/script.js
@@ -1,9 +1,7 @@
-var xmlFile = '../projects.xml';
-
 function loadXML() {
   var xhttp = new XMLHttpRequest();
 
-  xhttp.open("GET", xmlFile, true);
+  xhttp.open("GET", '../projects.xml', true);
   xhttp.send();
   xhttp.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
@@ -18,8 +16,8 @@ function xmlFunction(xml) {
   var xmlDoc = parser.parseFromString(xml, "text/xml");
   var table = "<tr><th>Titel</th></tr>";
   var x = xmlDoc.getElementsByTagName("projectList");
-  for (var elem of x) {
-    var titles = elem.getElementsByTagName(
+  for (i = 0; i < x.length; i++) {
+    var titles = x[i].getElementsByTagName(
       "title")[0].childNodes[0].nodeValue;
     table += "<tr><td>" + titles + "</td></tr>";
   }
