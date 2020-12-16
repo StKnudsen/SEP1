75b7b8ec8780c8f19f740f29623e57d7048634d9
StKnudsen <st.knudsen@gmail.com>
2020-12-16 07:52:39 +0100

    Lidt mere XML på hjemmeside

--- Web/js/project.js
+++ Web/js/project.js
@@ -3,7 +3,7 @@ function loadXML() {
 
   xhttp.open("GET", '../projects.xml', true);
   xhttp.send();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       xmlFunction(this.response);
     }
@@ -14,30 +14,41 @@ function loadXML() {
 function xmlFunction(xml) {
   var parser = new DOMParser();
   var xmlDoc = parser.parseFromString(xml, "text/xml");
-  var x = xmlDoc.getElementsByTagName("projectList");
-  var projectTitle = x[project].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+  var projectList = xmlDoc.getElementsByTagName("projectList");
+  var projectTitle = projectList[project].getElementsByTagName("title")[0].childNodes[0].nodeValue;
   document.getElementById("projectTitle").innerHTML = projectTitle;
+  // console.log("Current Project Number = " + project);
+
+  var table = "<thead class='thead-dark'>"
+
+  table += "<tr><th>Requirement titel</th><th>Type</th><th>Deadline</th><th>Status</th></tr></thead>";
+
+  var requirementList = projectList[project].getElementsByTagName("requirementList");
+  for (i = 0; i < requirementList.length; i++) {
+    var requirement = requirementList[i]; // Sætter requirement til at være et requirementList[i], som indeholder alle requirements
+    var requirementTitle = requirement.getElementsByTagName("title")[requirementList[i].getElementsByTagName("title").length - 1].innerHTML; // Sætter requirementTitle til den sidste title der findes. Da alle Tasks kommer før og også har en title, så skal de springes over først
+    var requirementType = requirement.getElementsByTagName("type")[requirementList[i].getElementsByTagName("type").length - 1].innerHTML;
+    //var requirementDeadline = requirement.getElementsByTagName("deadline")[requirementList[i].getElementsByTagName("deadline").length - 1].innerHTML;
+    var requirementStatus = requirement.getElementsByTagName("status")[requirementList[i].getElementsByTagName("status").length - 1].innerHTML; // Sætter requirementStatus til den sidste status der findes. samme grund som ovenfor
+    // console.log(requirementStatus);
+    table += "<tbody><tr><td>" + requirementTitle + "</td>"
+    table += "<td>" + requirementType + "</td>"
+    table += "<td>Deadline fra XML... Hvor?</td>"
+    table += "<td>" + requirementStatus + "</td></tr></tbody>" // Sætter Status ind i table
 
 
 
-  var table = "<tr><th>Requirement titel</th><th>Type</th><th>Deadline</th><th>Status</th></tr>";
+    // console.log(projectList[project].getElementsByTagName("title")[i + projectList[project].getElementsByTagName("taskList").length - 1]);
+    // console.log(projectList[project].getElementsByTagName("requirementList")[i].childNodes);
+    // console.log(projectList[project].getElementsByTagName("title")[i].childNodes[0].nodeValue);
+    //for (i = 0; i < projectList[project].getElementsByTagName("requirementList")[i].childNodes.title)
 
-    console.log(x[project].getElementsByTagName("requirementList"));  // første vi kan se
+    // var titles = projectList[project].getElementsByTagName("title")[i + projectList[project].getElementsByTagName("taskList").length - 1];
+    // var titles = projectList[project].getElementsByTagName("requirementList");
 
-  for (i = 0; i < x[project].getElementsByTagName("requirementList").length; i++) {
-
-    console.log(x[project].getElementsByTagName("title")[i + x[project].getElementsByTagName("taskList").length - 1] );
-    //console.log(x[project].getElementsByTagName("requirementList")[i].childNodes);
-    //console.log(x[project].getElementsByTagName("title")[i].childNodes[0].nodeValue);
-    //for (i = 0; i < x[project].getElementsByTagName("requirementList")[i].childNodes.title)
-
-    var titles = x[project].getElementsByTagName("title")[i + x[project].getElementsByTagName("taskList").length - 1];
-    //var titles = x[project].getElementsByTagName("requirementList");
-
-    table += "<tr><td>" + titles + "</td><td><a href='projekt.html?projekt=" + i + "'>Link</a></td></tr>";
+    // table += "<tr><td>" + titles + "</td><td><a href='projekt.html?projekt=" + i + "'>Link</a></td></tr>";
   }
   document.getElementById("requirementTable").innerHTML = table;
-
 
 }
 
@@ -46,10 +57,10 @@ var project = searchParams.get("projekt");
 
 loadXML();
 
-function lastSibling(node){
-    var tempObj=node.parentNode.lastChild;
-    while(tempObj.nodeType!=1 && tempObj.previousSibling!=null){
-    tempObj=tempObj.previousSibling;
-    }
-    return (tempObj.nodeType==1)?tempObj:false;
+function lastSibling(node) {
+  var tempObj = node.parentNode.lastChild;
+  while (tempObj.nodeType != 1 && tempObj.previousSibling != null) {
+    tempObj = tempObj.previousSibling;
+  }
+  return (tempObj.nodeType == 1) ? tempObj : false;
 }
--- Web/js/projects.js
+++ Web/js/projects.js
@@ -14,13 +14,17 @@ function loadXML() {
 function xmlFunction(xml) {
   var parser = new DOMParser();
   var xmlDoc = parser.parseFromString(xml, "text/xml");
-  var table = "<tr><th>Titel</th></tr>";
+  var table = "<thead class='thead-dark'><tr>";
+  table += "<th>Titel</th><th>Kunde</th><th>Link</th></tr>";
+  table += "</tr></thead><tbody>";
+
   var x = xmlDoc.getElementsByTagName("projectList");
   for (i = 0; i < x.length; i++) {
-    var titles = x[i].getElementsByTagName(
-      "title")[0].childNodes[0].nodeValue;
-    table += "<tr><td>" + titles + "</td><td><a href='projekt.html?projekt=" + i + "'>Link</a></td></tr>";
+    var titles = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+    var customers = x[i].getElementsByTagName("name")[x[i].getElementsByTagName("name").length - 1].childNodes[0].nodeValue;
+    table += "<tr><td>" + titles + "</td><td>" + customers + "</td><td><a href='projekt.html?projekt=" + i + "'>Link</a></td></tr>";
   }
+  table +="</tbody>"
   document.getElementById("projects").innerHTML = table;
 }
 loadXML();
