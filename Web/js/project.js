dcdab5dafea2a971b95a89922eac9b8585796fd7
StKnudsen <st.knudsen@gmail.com>
2020-12-16 08:22:09 +0100

    Lidt javascript opdydning, og masser af kommentarer

--- Web/js/project.js
+++ Web/js/project.js
@@ -1,3 +1,6 @@
+/*
+ *  Her henter vi xml filen der er genereret fra vores data
+ */
 function loadXML() {
   var xhttp = new XMLHttpRequest();
 
@@ -8,21 +11,27 @@ function loadXML() {
       xmlFunction(this.response);
     }
   };
-
 }
 
+/*
+ *  Her hiver vi den data fra XML filen ud, og indsætter
+ *  i en tabel, til visning på hjemmesiden.
+ */
 function xmlFunction(xml) {
   var parser = new DOMParser();
   var xmlDoc = parser.parseFromString(xml, "text/xml");
-  var projectList = xmlDoc.getElementsByTagName("projectList");
-  var projectTitle = projectList[project].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+
+  // Titlen på projektet tages her fra xml dokumentet og indsættes som projekt titel på hjemmesiden
+  projectList = xmlDoc.getElementsByTagName("projectList");
+  projectTitle = projectList[project].getElementsByTagName("title")[0].childNodes[0].nodeValue;
   document.getElementById("projectTitle").innerHTML = projectTitle;
-  // console.log("Current Project Number = " + project);
 
-  var table = "<thead class='thead-dark'>"
+  // HTML Table genereres og værdierne i headeren sættes ind
+  var table = "<thead class='thead-dark'><tr>"
+  table += "<th>Requirement titel</th><th>Type</th><th>Deadline</th><th>Status</th>";
+  table += "</tr></thead><tbody>";
 
-  table += "<tr><th>Requirement titel</th><th>Type</th><th>Deadline</th><th>Status</th></tr></thead>";
-
+  // Her findes de data i XML dokumentet der skal sendes præsenteres på hjemmesiden
   var requirementList = projectList[project].getElementsByTagName("requirementList");
   for (i = 0; i < requirementList.length; i++) {
     var requirement = requirementList[i]; // Sætter requirement til at være et requirementList[i], som indeholder alle requirements
@@ -30,37 +39,25 @@ function xmlFunction(xml) {
     var requirementType = requirement.getElementsByTagName("type")[requirementList[i].getElementsByTagName("type").length - 1].innerHTML;
     //var requirementDeadline = requirement.getElementsByTagName("deadline")[requirementList[i].getElementsByTagName("deadline").length - 1].innerHTML;
     var requirementStatus = requirement.getElementsByTagName("status")[requirementList[i].getElementsByTagName("status").length - 1].innerHTML; // Sætter requirementStatus til den sidste status der findes. samme grund som ovenfor
-    // console.log(requirementStatus);
-    table += "<tbody><tr><td>" + requirementTitle + "</td>"
-    table += "<td>" + requirementType + "</td>"
-    table += "<td>Deadline fra XML... Hvor?</td>"
-    table += "<td>" + requirementStatus + "</td></tr></tbody>" // Sætter Status ind i table
 
-
-
-    // console.log(projectList[project].getElementsByTagName("title")[i + projectList[project].getElementsByTagName("taskList").length - 1]);
-    // console.log(projectList[project].getElementsByTagName("requirementList")[i].childNodes);
-    // console.log(projectList[project].getElementsByTagName("title")[i].childNodes[0].nodeValue);
-    //for (i = 0; i < projectList[project].getElementsByTagName("requirementList")[i].childNodes.title)
-
-    // var titles = projectList[project].getElementsByTagName("title")[i + projectList[project].getElementsByTagName("taskList").length - 1];
-    // var titles = projectList[project].getElementsByTagName("requirementList");
-
-    // table += "<tr><td>" + titles + "</td><td><a href='projekt.html?projekt=" + i + "'>Link</a></td></tr>";
+    // Body på HTML Table genereres udfra de data fra XML dokumentet
+    table += "<tr><td>" + requirementTitle + "</td>";
+    table += "<td>" + requirementType + "</td>";
+    table += "<td>Deadline fra XML... Hvor?</td>";
+    table += "<td>" + requirementStatus + "</td></tr>";
   }
+  table += "</tbody>";
+  //  Den nu genereret tabel indsættes her på hjemmesiden
   document.getElementById("requirementTable").innerHTML = table;
-
 }
 
+/*
+ *  Her fanges hvilken parameter der er sendt med til hjemmesidens projekt side
+ *    /projekt.html/?projekt=<value>
+ *  Denne parameter værdi, bruges til at få data fra det korrekte projekt :)
+ */
 var searchParams = new URLSearchParams(window.location.search);
 var project = searchParams.get("projekt");
 
+// Start det ovenstående
 loadXML();
-
-function lastSibling(node) {
-  var tempObj = node.parentNode.lastChild;
-  while (tempObj.nodeType != 1 && tempObj.previousSibling != null) {
-    tempObj = tempObj.previousSibling;
-  }
-  return (tempObj.nodeType == 1) ? tempObj : false;
-}
--- Web/js/projects.js
+++ Web/js/projects.js
@@ -1,3 +1,6 @@
+/*
+ *  Her henter vi xml filen der er genereret fra vores data
+ */
 function loadXML() {
   var xhttp = new XMLHttpRequest();
 
@@ -8,23 +11,34 @@ function loadXML() {
       xmlFunction(this.response);
     }
   };
-
 }
 
+/*
+ *  Her hiver vi den data fra XML filen ud, og indsætter
+ *  i en tabel, til visning på hjemmesiden.
+ */
 function xmlFunction(xml) {
   var parser = new DOMParser();
   var xmlDoc = parser.parseFromString(xml, "text/xml");
+
+  // HTML Table genereres og værdierne i headeren sættes ind
   var table = "<thead class='thead-dark'><tr>";
   table += "<th>Titel</th><th>Kunde</th><th>Link</th></tr>";
   table += "</tr></thead><tbody>";
 
-  var x = xmlDoc.getElementsByTagName("projectList");
-  for (i = 0; i < x.length; i++) {
-    var titles = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-    var customers = x[i].getElementsByTagName("name")[x[i].getElementsByTagName("name").length - 1].childNodes[0].nodeValue;
+// Her findes de data i XML dokumentet der skal sendes præsenteres på hjemmesiden
+  var projectList = xmlDoc.getElementsByTagName("projectList");
+  for (i = 0; i < projectList.length; i++) {
+    var titles = projectList[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+    var customers = projectList[i].getElementsByTagName("name")[projectList[i].getElementsByTagName("name").length - 1].childNodes[0].nodeValue;
+
+    // Body på HTML Table genereres udfra de data fra XML dokumentet
     table += "<tr><td>" + titles + "</td><td>" + customers + "</td><td><a href='projekt.html?projekt=" + i + "'>Link</a></td></tr>";
   }
   table +="</tbody>"
+  // Den nu genereret tabel indsættes her på hjemmesiden
   document.getElementById("projects").innerHTML = table;
 }
+
+// Start det ovenstående
 loadXML();
